fe2d0fe3f29b8578f850dd2c87d235213116f992
Luis Sandoval <luismsandoval96@gmail.com>
2022-04-07 19:26:35 -0700

    added constructor function

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//global variables
+
+//constructor function
+
+function Destination(name, climate, budget, size) {
+  this.name = name;
+  this.climate = climate;
+  this.budget = budget;
+  this.size = size;
+}
